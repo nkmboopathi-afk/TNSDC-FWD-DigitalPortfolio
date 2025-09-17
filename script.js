55bde31ad93cb4ade7214207f5eaed67f5cb951c
nkmboopathi-afk <nkmboopathi@gmail.com>
2025-09-17 15:57:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("formMessage").textContent = "Thank you! Your message has been sent.";
-  document.getElementById("formMessage").style.color = "#ff1493";
-  this.reset();
-});
